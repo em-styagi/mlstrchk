9450c20f7c1301c43a01559cc755b84fc7948149
willbert <willbert@halsey.halsey.com>
2018-05-14 23:55:21 +0100

    added check/pass/fail count, added final method
    Changes in this commit:
     - added counts to record #tests #passes #fails
     - added method to determine if numbers 1-12 used in string

--- MlStringCheck.java
+++ MlStringCheck.java
@@ -43,29 +43,56 @@ public class MlStringCheck {
         }
     }
 
+    public boolean noNumbersBelowThirteen() {
+        //Numbers below 13 are spelled out (”one”, “two”, "three”, etc…)
+        /* is it sufficient to check for the numbers 1-12 within
+           the string and have those strings which contain these
+           numbers fail?
+           how do we know if the spelling of a number is correct?
+           how do we know if the intent was to have a number? */
+        if (testString.matches("(.*[0-9,10,11,12].*)")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     public static void main (String[] args) {
         MlStringCheck mysc = new MlStringCheck("The quick brown fox said \"hello Mr lazy dog\".");
 
         int passCount = 0;
+        int testCount = 0;
 
+        testCount++;
         if (mysc.startsWithCapital()) {
             passCount++;
             System.out.println("passed");
         }
 
+        testCount++;
         if (mysc.endsWithPeriod()) {
             passCount++;
             System.out.println("passed");
         }
 
+        testCount++;
         if (mysc.containsEvenCountOfQuotes()) {
             passCount++;
             System.out.println("passed");
         }
 
+        testCount++;
         if (mysc.containsNoPeriodOtherThanEnd()) {
             passCount++;
             System.out.println("passed");
         }
+
+        testCount++;
+        if (mysc.noNumbersBelowThirteen()){
+            passCount++;
+            System.out.println("passed");
+        }
+
+        System.out.println("Checks: " + testCount + " Passed: " + passCount + " Failed: " + (testCount-passCount));
     }
 }
